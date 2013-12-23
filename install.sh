268d3c8981676680b149078494dd7416a2dc2688
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-23 21:17:21 -0200

    refactorings suggested by ShellChecker, quotings, etc.

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 # the way to find out the full REAL path to the dir where THIS script lives
-REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
+REL_SRC="${BASH_SOURCE[0]}"
+CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 
 # making a backup of the current screenrc the user might have
 TARGET="${HOME}/.screenrc"
-if [ -e ${TARGET} ]; then  # only backup if it exists
-    if [ ! -L ${TARGET} ]; then  # only if it is NOT a link
-        mv ${TARGET} "${TARGET}.bkp"
+if [ -e "${TARGET}" ]; then  # only backup if it exists
+    if [ ! -L "${TARGET}" ]; then  # only if it is NOT a link
+        mv "${TARGET}" "${TARGET}.bkp"
     fi
 fi
 
 # making the link to the version in this repo
-echo "source ${DIR}/screenrc" > ${TARGET}
+echo "source ${DIR}/screenrc" > "${TARGET}"
 
 # link to screen-scripts dir
 ln -s -f "${DIR}/screen-scripts"   "${HOME}/.screen-scripts"
+
